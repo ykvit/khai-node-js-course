cc7dddf906db64d6afc06339144cbb39b98a19ee
Illia O. <yumkvitka+github@gmail.com>
2025-04-07 19:14:58 +0300

    setup-db

---
+++ src/index.js
@@ -0,0 +1,15 @@
+const express = require('express')
+const productRoutes = require('./product.routes');
+const { logRequest } = require('./middleware');
+const { errorResponder } = require('./error.middleware');
+
+const app = express();
+const PORT = 3000;
+
+app.use(logRequest);
+app.use(productRoutes);
+app.use(errorResponder);
+
+app.listen(PORT, () => {
+  console.log(`Server listening at http://localhost:${PORT}`);
+});
